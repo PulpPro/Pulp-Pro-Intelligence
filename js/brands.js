f6e2e62f6f7c975034291141b56e2a0e26d486c5
Pulp Pro Intellignece <akash.risma@gmail.com>
2026-05-05 11:50:54 +0200

    Create brands.js

---
+++ js/brands.js
@@ -0,0 +1,22 @@
+// Open Brand Hub
+function openBrands(fruit) {
+    activeFruit = fruit;
+    switchView('brand-hub');
+    const grid = document.getElementById('brandGrid');
+
+    if (fruit === 'banana') {
+        grid.innerHTML = `
+            <div class="list-btn" onclick="openCalc('Chiquita')">Chiquita</div>
+            <div class="list-btn disabled">Fyffes (Soon)</div>
+            <div class="list-btn disabled">Favorita (Soon)</div>
+            <div class="list-btn disabled">Agrofair (Soon)</div>
+            <div class="list-btn disabled">Port (Soon)</div>
+        `;
+    } else {
+        grid.innerHTML = `
+            <div style="opacity:0.5; padding:20px;">
+                Logic for ${fruit} brands coming soon.
+            </div>
+        `;
+    }
+}
